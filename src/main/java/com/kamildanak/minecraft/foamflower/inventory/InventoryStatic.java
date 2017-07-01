c7b0a12d4a3802fe74453455642ef90d96782252
Kamil Danak <kamildanak@gmail.com>
2017-07-01 03:35:28 +0200

    InventoryStatic extends ItemStackHandler

--- src/main/java/com/kamildanak/minecraft/foamflower/inventory/InventoryStatic.java
+++ src/main/java/com/kamildanak/minecraft/foamflower/inventory/InventoryStatic.java
@@ -1,26 +1,86 @@
 package com.kamildanak.minecraft.foamflower.inventory;
 
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.item.Item;
+import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.items.ItemHandlerHelper;
+import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
-import java.util.Objects;
 
-public abstract class InventoryStatic implements IInventory {
-    public final NonNullList<ItemStack> items;
-
+public class InventoryStatic extends ItemStackHandler implements IInventory {
     public InventoryStatic(int size) {
-        items = NonNullList.withSize(size, ItemStack.EMPTY);
+        super(size);
+    }
+
+    @Override
+    public void deserializeNBT(NBTTagCompound nbt) {
+        setSize(nbt.hasKey("Size", Constants.NBT.TAG_INT) ? nbt.getInteger("Size") : stacks.size());
+        NBTTagList tagList = nbt.getTagList("Items", Constants.NBT.TAG_COMPOUND);
+        for (int i = 0; i < tagList.tagCount(); i++) {
+            NBTTagCompound itemTags = tagList.getCompoundTagAt(i);
+            int slot = itemTags.getByte("slot") & 0xff;
+
+            if (slot >= 0 && slot < stacks.size()) {
+                stacks.set(slot, new ItemStack(itemTags));
+            }
+        }
+        onLoad();
+    }
+
+    @Override
+    public NBTTagCompound serializeNBT() {
+        NBTTagList nbtTagList = new NBTTagList();
+        for (int i = 0; i < stacks.size(); i++) {
+            if (!stacks.get(i).isEmpty()) {
+                NBTTagCompound itemTag = new NBTTagCompound();
+                itemTag.setByte("slot", (byte) i);
+                stacks.get(i).writeToNBT(itemTag);
+                nbtTagList.appendTag(itemTag);
+            }
+        }
+        NBTTagCompound nbt = new NBTTagCompound();
+        nbt.setTag("Items", nbtTagList);
+        nbt.setInteger("Size", stacks.size());
+        return nbt;
+    }
+
+    @Override
+    public void setInventorySlotContents(int i, @Nonnull ItemStack itemstack) {
+        super.setStackInSlot(i, itemstack);
+    }
+
+    public boolean isEmpty() {
+        for (ItemStack item : stacks) {
+            if (!item.isEmpty()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public ItemStack insertItem(ItemStack paid, int start, int end, boolean simulate) {
+        for (int i = start; i <= end; i++) {
+            paid = this.insertItem(i, paid, simulate);
+        }
+        return paid;
+    }
+
+    public ItemStack extractItem(ItemStack sold, int count, int start, int end, boolean simulate) {
+        int expected = count;
+        for (int i = start; i <= end; i++) {
+            if (sold.isItemEqual(this.extractItem(i, count, true))) {
+                count -= this.extractItem(i, count, simulate).getCount();
+            }
+        }
+        return ItemHandlerHelper.copyStackWithSize(sold, expected - count);
     }
 
     @Override
@@ -35,6 +95,7 @@ public abstract class InventoryStatic implements IInventory {
         return new TextComponentTranslation("");
     }
 
+
     @Override
     public boolean hasCustomName() {
         return true;
@@ -45,240 +106,32 @@ public abstract class InventoryStatic implements IInventory {
         return false;
     }
 
-    public void onInventoryChanged(int slot) {
-    }
-
     @Override
     public int getSizeInventory() {
-        return items.size();
+        return stacks.size();
     }
 
     @Override
     @Nonnull
-    public ItemStack getStackInSlot(int i) {
-        return items.get(i);
+    public ItemStack decrStackSize(int index, int amount) {
+        return ItemStackHelper.getAndSplit(this.stacks, index, amount);
     }
 
     @Override
     @Nonnull
-    public ItemStack decrStackSize(int i, int j) {
-        if (!items.get(i).isEmpty()) {
-            if (items.get(i).getCount() <= j) {
-                ItemStack itemstack = items.get(i);
-                items.set(i, ItemStack.EMPTY);
-                onInventoryChanged();
-                onInventoryChanged(i);
-                return itemstack;
-            }
-
-            ItemStack itemstack1 = items.get(i).splitStack(j);
-
-            if (items.get(i).getCount() == 0) {
-                items.set(i, ItemStack.EMPTY);
-            }
-
-            onInventoryChanged();
-            onInventoryChanged(i);
-            return itemstack1;
-        } else {
-            return ItemStack.EMPTY;
-        }
+    public ItemStack removeStackFromSlot(int index) {
+        return ItemStackHelper.getAndRemove(this.stacks, index);
     }
 
-    @Override
-    public void setInventorySlotContents(int i, @Nonnull ItemStack itemstack) {
-        items.set(i, itemstack);
-
-        if (!itemstack.isEmpty() && itemstack.getCount() > getInventoryStackLimit()) {
-            itemstack.setCount(getInventoryStackLimit());
-        }
-
-        onInventoryChanged();
-        onInventoryChanged(i);
-    }
 
     @Override
     public int getInventoryStackLimit() {
         return 64;
     }
 
-    public void readFromNBT(NBTTagCompound nbtTagCompound) {
-        NBTTagList nbtTagList = nbtTagCompound.getTagList("Items", Constants.NBT.TAG_COMPOUND);
-        for (int i = 0; i < nbtTagList.tagCount(); ++i) {
-            NBTTagCompound nbtTagCompound1 = nbtTagList.getCompoundTagAt(i);
-            int j = nbtTagCompound1.getByte("slot") & 0xff;
+    @Override
+    public void markDirty() {
 
-            items.set(j, new ItemStack(nbtTagCompound1));
-        }
-
-        onInventoryChanged();
-    }
-
-    public void writeToNBT(NBTTagCompound nbttagcompound) {
-        NBTTagList nbttaglist = new NBTTagList();
-
-        for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).isEmpty()) {
-                continue;
-            }
-
-            NBTTagCompound nbttagcompound1 = new NBTTagCompound();
-            nbttagcompound1.setByte("slot", (byte) i);
-            items.get(i).writeToNBT(nbttagcompound1);
-            nbttaglist.appendTag(nbttagcompound1);
-        }
-
-        nbttagcompound.setTag("Items", nbttaglist);
-    }
-
-    private int getFirstEmptyStack(int start, int end) {
-        for (int i = start; i <= end; i++) {
-            if (items.get(i).isEmpty()) {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-
-    private int storeItemStack(ItemStack itemstack, int start, int end) {
-        for (int i = start; i <= end; i++) {
-            if (!items.get(i).isEmpty() &&
-                    items.get(i).getItem() == itemstack.getItem() &&
-                    items.get(i).isStackable() &&
-                    items.get(i).getCount() < items.get(i).getMaxStackSize() &&
-                    items.get(i).getCount() < getInventoryStackLimit() &&
-                    (!items.get(i).getHasSubtypes() || items.get(i).getItemDamage() == itemstack.getItemDamage())) {
-                if (items.get(i).hasTagCompound() || itemstack.hasTagCompound()) {
-                    if (items.get(i).hasTagCompound() && itemstack.hasTagCompound()) {
-                        if (Objects.equals(items.get(i).getTagCompound(), itemstack.getTagCompound())) {
-                            return i;
-                        }
-                    }
-                } else {
-                    return i;
-                }
-            }
-        }
-
-        return -1;
-    }
-
-    private int storePartialItemStack(ItemStack itemstack, int start, int end) {
-        Item i = itemstack.getItem();
-        int j = itemstack.getCount();
-        int k = storeItemStack(itemstack, start, end);
-
-        if (k < 0) {
-            k = getFirstEmptyStack(start, end);
-        }
-
-        if (k < 0) {
-            return j;
-        }
-
-        if (items.get(k).isEmpty()) {
-            items.set(k, new ItemStack(i, 0, itemstack.getItemDamage()));
-            if (itemstack.hasTagCompound()) {
-                items.get(k).setTagCompound(itemstack.getTagCompound());
-            }
-        }
-
-        int l = j;
-
-        if (l > items.get(k).getMaxStackSize() - items.get(k).getCount()) {
-            l = items.get(k).getMaxStackSize() - items.get(k).getCount();
-        }
-
-        if (l > getInventoryStackLimit() - items.get(k).getCount()) {
-            l = getInventoryStackLimit() - items.get(k).getCount();
-        }
-
-        if (l == 0) {
-            return j;
-        } else {
-            j -= l;
-            items.get(k).setCount(items.get(k).getCount() + l);
-            items.get(k).setAnimationsToGo(5);
-            onInventoryChanged();
-            onInventoryChanged(k);
-            return j;
-        }
-    }
-
-    public boolean addItemStackToInventory(@Nonnull ItemStack itemstack, int start, int end) {
-        if (itemstack.isEmpty()) {
-            return true;
-        }
-        if (!itemstack.isItemDamaged()) {
-            int i;
-            do {
-                i = itemstack.getCount();
-                itemstack.setCount(storePartialItemStack(itemstack, start, end));
-            } while (itemstack.getCount() > 0 && itemstack.getCount() < i);
-
-            return itemstack.getCount() < i;
-        }
-        int j = getFirstEmptyStack(start, end);
-
-        if (j >= 0) {
-            items.set(j, itemstack.copy());
-            if (itemstack.hasTagCompound()) {
-                items.get(j).setTagCompound(itemstack.getTagCompound());
-            }
-            items.get(j).setAnimationsToGo(5);
-            itemstack.setCount(0);
-            onInventoryChanged();
-            onInventoryChanged(j);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean addItemStackToInventory(ItemStack itemstack) {
-        return addItemStackToInventory(itemstack, 0, items.size() - 1);
-    }
-
-    public ItemStack takeItems(ItemStack itemStack, int damage, int count) {
-        ItemStack res = ItemStack.EMPTY;
-
-        for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).isEmpty() || items.get(i).getItem() != itemStack.getItem() || items.get(i).getItemDamage() != damage) {
-                continue;
-            }
-
-            if (itemStack.hasTagCompound()) {
-                if (!Objects.equals(itemStack.getTagCompound(), items.get(i).getTagCompound())) {
-                    continue;
-                }
-            }
-            if (res.isEmpty()) {
-                res = new ItemStack(itemStack.getItem(), 0, damage);
-            }
-
-            while (!items.get(i).isEmpty() && res.getCount() < count && items.get(i).getCount() > 0) {
-                res.setCount(res.getCount() + 1);
-                items.get(i).setCount(items.get(i).getCount() - 1);
-
-                if (items.get(i).getCount() == 0) {
-                    items.set(i, ItemStack.EMPTY);
-                }
-
-                onInventoryChanged(i);
-            }
-
-            if (res.getCount() >= count) {
-                break;
-            }
-        }
-
-        onInventoryChanged();
-        return res;
-    }
-
-    //@Override
-    public void onInventoryChanged() {
     }
 
     @Override
@@ -309,50 +162,9 @@ public abstract class InventoryStatic implements IInventory {
         return true;
     }
 
-    public boolean isEmpty() {
-        for (ItemStack item : items) {
-            if (!item.isEmpty()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     public void clear() {
-        for (int i = 0; i < items.size(); i++) {
-            items.set(i, ItemStack.EMPTY);
+        for (int i = 0; i < stacks.size(); i++) {
+            stacks.set(i, ItemStack.EMPTY);
         }
-    }
-
-    public void throwItems(World world, int x, int y, int z) {
-        if (world.isRemote) return;
-
-        for (int i = 0; i < items.size(); i++) {
-            ItemStack itemstack = items.get(i);
-            if (itemstack.isEmpty()) continue;
-
-            items.set(i, ItemStack.EMPTY);
-
-            float xx = world.rand.nextFloat() * 0.8F + 0.1F;
-            float yy = world.rand.nextFloat() * 0.8F + 0.1F;
-            float zz = world.rand.nextFloat() * 0.8F + 0.1F;
-            while (itemstack.getCount() > 0) {
-                int c = world.rand.nextInt(21) + 10;
-                if (c > itemstack.getCount()) {
-                    c = itemstack.getCount();
-                }
-
-                itemstack.setCount(itemstack.getCount() - c);
-                EntityItem entityitem = new EntityItem(world, x + xx, y + yy, z + zz, new ItemStack(itemstack.getItem(), c, itemstack.getItemDamage()));
-                float f3 = 0.05F;
-                entityitem.motionX = (float) world.rand.nextGaussian() * f3;
-                entityitem.motionY = (float) world.rand.nextGaussian() * f3 + 0.2F;
-                entityitem.motionZ = (float) world.rand.nextGaussian() * f3;
-                world.spawnEntity(entityitem);
-            }
-        }
-
-        onInventoryChanged();
     }
 }
